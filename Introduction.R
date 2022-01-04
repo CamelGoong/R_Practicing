849c5baf329a16d8da7bcde6d54fc8789c070927
CamelGoong <goong0405@gmail.com>
2022-01-04 21:25:31 +0900

    Add files via upload

---
+++ Introduction.R
@@ -0,0 +1,37 @@
+a = 1
+
+a
+a<-1
+a
+a <-2 # 숫자형
+a
+a==1a
+a==2
+a!=1
+a .1
+a>
+a<-"abc"
+a <- "abc" # 문자형
+
+"asdfasdfdsafasdfdsa"
+a <- 3
+class(a)
+
+b <-"abcd"
+class(b)
+
+a2 <- as.character(a)
+class(a2)
+a2
+
+a2 <- 3
+as.character(a2)
+class(a2)
+
+as.numeric(a2)
+class(a2)
+as.character(a2)
+class(a2)
+
+a2 <- as.character(a2)
+class(a2)
